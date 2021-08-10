fe1f93c08980a0dfc7f6addc1cdb9252675d5ee8
Sweeney Ngo <sweeneyngo@gmail.com>
2021-08-10 00:27:01 -0700

    Swapped mocha for jest + testing library

--- frontend/src/components/Chevron/Chevon.js
+++ frontend/src/components/Chevron/Chevon.js
@@ -4,7 +4,10 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const Chevron = () => {
     return (
-        <div className="text-gray-500 my-1 text-xs flex flex-col items-center justify-center">
+        <div
+            data-testid="chevron"
+            className="text-gray-500 my-1 text-xs flex flex-col items-center justify-center"
+        >
             scroll down
             <div className="pb-0 text-gray-400">
                 <FontAwesomeIcon icon={faChevronDown} />
