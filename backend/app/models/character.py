af881e44f61b45205ce7543997323482095d24a5
Sweeney Ngo <sweeneyngo@gmail.com>
2021-08-19 19:28:27 -0700

    Added new models

---
+++ backend/app/models/character.py
@@ -0,0 +1,18 @@
+from app.shared.models import db
+
+class CharacterModel(db.Model):
+    __tablename__ = 'character'
+
+    id_character = db.Column(db.Integer, primary_key=True)
+    id_topic = db.Column(db.Integer, foreign_key=True)
+    name = db.Column(db.String()),
+    files = db.relationship('FileModel', backref='character', lazy=True)
+
+
+
+    def __init__(self, id_topic, name):
+        self.id_topic = id_topic
+        self.name = name
+
+    def __repr__(self):
+        return f"<File {self.name}>"
